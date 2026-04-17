ef1f75880f22b04aba207f6e862a26d19dbefc84
Luciano <luciano.santos.nascimento@outlook.com>
2026-04-17 12:21:23 -0400

    A1: 2v Atividade de luciano dos santos nascimento - noturno

---
+++ Atividades-A1/Noturno/Luciano-dos-Santos-Nascimento/nivel_dois_intermediario/src/app.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json());
+
+let alunos = []
+
+app.use((req, res, next) => {
+  console.log("Luciano passou no middleware");
+  next();
+});+
+
+app.post("/inserir", (req, res)=>{
+    let novoAluno = {
+        id: alunos.length +1,
+        nome: req.body.nome,
+        idade: req.body.idade,
+        curso: req.body.curso,
+    }
+
+    alunos.push(novoAluno)
+    res.status(201).json(novoAluno)
+})
+
+app.get("/alunos", (req, res) =>{
+    res.json(alunos)
+})
+
+app.put("/aluno/:id", (req, res)=>{
+  
+    let ID = req.params.id
+    alunos[ID] = {
+        nome: req.body.nome,
+        idade: req.body.idade,
+        curso: req.body.curso,
+    }
+
+    res.status(200).json(alunos[ID])
+
+})
+
+app.delete("/aluno/:id", (req, res)=>{
+  
+ 
+    alunos.splice(req.params.id, 1)
+
+    res.status(200).send("Removido!!!!")
+    
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
